ab59ea5bfad9472168d44f7376465db2345b3e05
badr21b <badreddine@pembepanjur.com>
2022-04-20 04:46:37 +0300

    - header styling => wip

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,15 @@
 import {isBrowser} from 'react-device-detect';
 import logo from './logo.svg';
+import { BsTelephone } from 'react-icons/bs';
 
 
 const HeaderComp = () => {
     return (
         <>
-            <div className={"social-media-holder"}>
-                <div className={"social-media-container"}>
-                    <div className={"social-media-item-container"}>Social media here</div>
-                    <div className={"social-media-item-container"}>Location here</div>
-                    <div className={"social-media-item-container"}>Contact here</div>
-                </div>
-            </div>
+            {(isBrowser) && (
+                <HeaderSocialMedia />
+            )}
+
             <div>
                 <img src={logo} className="App-logo" alt="logo" />
                 <div>{(!isBrowser)?("Is Mobile"): ("Is Desktop")}</div>
@@ -20,5 +18,27 @@ const HeaderComp = () => {
 
     )
 }
+
+const HeaderSocialMedia = () => {
+    return (
+        <div className={"social-media-holder"}>
+            <div className={"social-media-container"}>
+                <div className={"social-media-item-container"} style={{width: "calc(75% - 24px)", justifyContent: "flex-start"}}>
+                    <div className={"social-media-item"}>
+                        <BsTelephone />
+                        <div className={"phoneNumber"}>+90 531 123 45 67</div>
+                    </div>
+
+                    <div className={"social-media-item"}>
+                        Location here
+                    </div>
+                </div>
+
+                <div className={"social-media-item-container"} style={{width: "25%", justifyContent: "flex-end"}}>Contact here</div>
+            </div>
+        </div>
+    )
+}
+
 
 export default HeaderComp;
