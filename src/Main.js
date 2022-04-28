eeb6137dac33aa061ae253f236be192922fe27b0
badr21b <badreddine@pembepanjur.com>
2022-04-28 04:04:20 +0300

    - header styling => wip - router => wip^

--- src/Main.js
+++ src/Main.js
@@ -1,25 +1,25 @@
-import {GrFacebookOption} from "react-icons/gr";
+import {GoPrimitiveDot} from "react-icons/go";
 
 export default function MainComp() {
     const treatmentList = [
-        {key: "dandruffTreatment", text: "Dandruff Treatment", icon: <GrFacebookOption className={"icon"}/>},
-        {key: "HairThinning", text: "Hair Thinning", icon: <GrFacebookOption className={"icon"}/>},
-        {key: "RoughHairTreatment", text: "Rough hair treatment", icon: <GrFacebookOption className={"icon"}/>},
-        {key: "Alopecia", text: "Alopecia", icon: <GrFacebookOption className={"icon"}/>},
-        {key: "HairTossTreatment", text: "Hair loss treatment", icon: <GrFacebookOption className={"icon"}/>},
-        {key: "AlopeciaAreata", text: "Alopecia Areata", icon: <GrFacebookOption className={"icon"}/>},
+        {key: "dandruffTreatment", text: "Dandruff Treatment", icon: <GoPrimitiveDot className={"icon"}/>},
+        {key: "HairThinning", text: "Hair Thinning", icon: <GoPrimitiveDot className={"icon"}/>},
+        {key: "RoughHairTreatment", text: "Rough hair treatment", icon: <GoPrimitiveDot className={"icon"}/>},
+        {key: "Alopecia", text: "Alopecia", icon: <GoPrimitiveDot className={"icon"}/>},
+        {key: "HairTossTreatment", text: "Hair loss treatment", icon: <GoPrimitiveDot className={"icon"}/>},
+        {key: "AlopeciaAreata", text: "Alopecia Areata", icon: <GoPrimitiveDot className={"icon"}/>},
     ]
     return (
         <main style={{ padding: "1rem 0" }}>
             <div>
                 <div>#1 Ranked Hair Clinic in Turkey</div>
                 <div>Life isn't perfect but Your hair can be.</div>
-                <div>
+                <div className={"treatmentListContainer"}>
                     {treatmentList.map(item => {
                       return(
-                          <div key={item.key} >
+                          <div key={item.key} className={"treatmentItem"}>
                               {item.icon}
-                              <div>{item.text}</div>
+                              <div className={"treatmentItemText"}>{item.text}</div>
                           </div>
                       )
                     })}
