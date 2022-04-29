8518e411deb60f05c8aee5411f47ad32565464c7
badr21b <badreddine@pembepanjur.com>
2022-04-29 10:34:29 +0300

    - Main page => feeling up

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,21 @@
 import {GoPrimitiveDot} from "react-icons/go";
+import {FaHandHoldingMedical} from "react-icons/fa";
+import {BsTelephone} from "react-icons/bs";
 
 export default function MainComp() {
     const treatmentList = [
-        {key: "dandruffTreatment", text: "Dandruff Treatment", icon: <GoPrimitiveDot className={"icon"}/>},
-        {key: "HairThinning", text: "Hair Thinning", icon: <GoPrimitiveDot className={"icon"}/>},
-        {key: "RoughHairTreatment", text: "Rough hair treatment", icon: <GoPrimitiveDot className={"icon"}/>},
-        {key: "Alopecia", text: "Alopecia", icon: <GoPrimitiveDot className={"icon"}/>},
-        {key: "HairTossTreatment", text: "Hair loss treatment", icon: <GoPrimitiveDot className={"icon"}/>},
-        {key: "AlopeciaAreata", text: "Alopecia Areata", icon: <GoPrimitiveDot className={"icon"}/>},
+        {key: "dandruffTreatment", text: "Dandruff Treatment", icon: <FaHandHoldingMedical className={"treatmentItemIcon"}/>},
+        {key: "HairThinning", text: "Hair Thinning", icon: <FaHandHoldingMedical className={"treatmentItemIcon"}/>},
+        {key: "RoughHairTreatment", text: "Rough hair treatment", icon: <FaHandHoldingMedical className={"treatmentItemIcon"}/>},
+        {key: "Alopecia", text: "Alopecia", icon: <FaHandHoldingMedical className={"treatmentItemIcon"}/>},
+        {key: "HairTossTreatment", text: "Hair loss treatment", icon: <FaHandHoldingMedical className={"treatmentItemIcon"}/>},
+        {key: "AlopeciaAreata", text: "Alopecia Areata", icon: <GoPrimitiveDot className={"treatmentItemIcon"}/>},
     ]
     return (
         <main style={{ padding: "1rem 0" }}>
-            <div>
-                <div>#1 Ranked Hair Clinic in Turkey</div>
-                <div>Life isn't perfect but Your hair can be.</div>
+            <div className={"textParagraphContainer"}>
+                <div className={"mainParagraphTitle"}>#1 Ranked Hair Clinic in Turkey</div>
+                <div className={"mainParagraphSubtitle"}>Life isn't perfect but Your hair can be.</div>
                 <div className={"treatmentListContainer"}>
                     {treatmentList.map(item => {
                       return(
@@ -25,13 +27,15 @@ export default function MainComp() {
                     })}
                 </div>
 
-                <div>
-                    <div>Make an appointment</div>
-                    <div>Phone +90 123456789</div>
+                <div className={"buttonGroupContainer"}>
+                    <div className={"buttonItem"}>Make an appointment</div>
+                    <div className={"buttonItem"}>
+                        <BsTelephone style={{marginRight: "9px"}}/>
+                        +90 (123) 145-6789
+                    </div>
                 </div>
-
-                <img src={"https://assets.website-files.com/620a7612af716dbffa6c4a61/620cc9eb17d49233903dd591_Hero-image-hair-treatment-min.png"}  alt={""}/>
             </div>
+            <img style={{width: "calc(35% + 36px)"}} src={"https://assets.website-files.com/620a7612af716dbffa6c4a61/620cc9eb17d49233903dd591_Hero-image-hair-treatment-min.png"}  alt={""}/>
         </main>
     );
 }
