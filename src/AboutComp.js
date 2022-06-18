661aa94bbd3efd91b88b299dc881d8ad20f609d2
badr21b <badreddine@pembepanjur.com>
2022-06-18 03:49:11 +0300

    styling => wip

--- src/AboutComp.js
+++ src/AboutComp.js
@@ -43,57 +43,24 @@ export default function AboutComp(props) {
 
                 <div className={styles.background} />
 
-                <Parallax pages={treatmentsList.length * 2}>
+                <Parallax pages={10}>
 
                     <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
                         {/*<p className={styles.scrollText}>Scroll down</p>*/}
                         <div className={(isDesktop)? ("blocksContainer"):("blocksContainerMobile")} style={{height: "100vh", width: "100%"}}>
                             {/*<div className={"backgroundDetailsContainer"}/>*/}
                             <HashLink smooth className={"menu-list-item"} key={"hair"} to={"#hair"}> Hair anchor</HashLink>
-                            <HashLink smooth className={"menu-list-item"} key={"hair"} to={"#dent"}> Dent anchor</HashLink>
+                            <HashLink smooth className={"menu-list-item"} key={"hair"} to={"#dent"}> dent anchor</HashLink>
+                            <HashLink smooth className={"menu-list-item"} key={"hair"} to={"#aesthetic"}> aesthetic anchor</HashLink>
                         </div>
                     </ParallaxLayer>
 
-                    {/*{treatmentsList.map((category, key) => {*/}
-                    {/*    return (*/}
-                    {/*       <>*/}
-                    {/*           <ParallaxLayer sticky={{ start: 1, end: 3 }} style={{ ...alignCenter, justifyContent: 'flex-start' }} >*/}
-                    {/*               <div className={`${styles.card} ${styles.sticky} ${styles.verticalGroup}`}>*/}
 
-                    {/*                   <div className={"backgroundDetailsContainer"} style={{backgroundImage: categoryObj.backgroundUrl}}>*/}
-                    {/*                       <div>*/}
-                    {/*                           <p>Greffe de Cheveux</p>*/}
-                    {/*                           <div>*/}
-                    {/*                               Transplantation Cheveux*/}
-                    {/*                               La perte de cheveux est acceptée comme un problème majeur affectant un pourcentage important de la population. Bien qu’il existe diverses raisons contribuant à la perte de cheveux, la cause la plus importante reste l’alopécie androgénétique qui est une affection héréditaire associée à la sensibilité des androgènes. Alors que les traitements non chirurgicaux disponibles peuvent aider à ralentir la perte de cheveux dans une certaine mesure, des progrès sans fin sur la restauration chirurgicale des cheveux ont transformé la greffe de cheveux en une méthode presque sûre et efficace, impeccable pour les patients affectés.*/}
 
-                    {/*                               La greffe de cheveux est un type de procédure en évolution constante, fournissant des solutions aux patients aux prises avec l’alopécie androgénétique ainsi que la perte de cheveux résultant d’autres sources telles que les traumatismes ou les troubles inflammatoires des cheveux. Les résultats d’apparence naturelle obtenus lors des opérations de greffe de cheveux agissent comme des facteurs encourageants, et chaque jour qui passe, de plus en plus d’hommes et de femmes optent pour la restauration des cheveux.*/}
-
-                    {/*                           </div>*/}
-                    {/*                       </div>*/}
-                    {/*                   </div>*/}
-                    {/*               </div>*/}
-                    {/*           </ParallaxLayer>*/}
-
-                    {/*           <ParallaxLayer offset={1.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>*/}
-                    {/*               <div className={`${styles.card} ${styles.parallax} ${styles.blue_2}`}>*/}
-                    {/*                   <p>Transplantation Cheveux</p>*/}
-                    {/*               </div>*/}
-                    {/*           </ParallaxLayer>*/}
-
-                    {/*           <ParallaxLayer offset={2.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>*/}
-                    {/*               <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>*/}
-                    {/*                   <p>Neither am I</p>*/}
-                    {/*               </div>*/}
-                    {/*           </ParallaxLayer>*/}
-                    {/*       </>*/}
-                    {/*    )*/}
-                    {/*} )}*/}
-
-                    <ParallaxLayer  id='hair' sticky={{ start: 1, end: 3 }} style={{ ...alignCenter, justifyContent: 'flex-start' }} >
+                    <ParallaxLayer sticky={{ start: 1, end: 3 }} style={{ ...alignCenter, justifyContent: 'flex-start' }} >
                         <div className={`${styles.card} ${styles.sticky} ${styles.verticalGroup}`}>
 
-                            <div className={"backgroundDetailsContainer"} style={{backgroundImage: categoryObj.backgroundUrl}}>
+                            <div  id='hair' className={"backgroundDetailsContainer"} style={{backgroundImage: categoryObj.backgroundUrl}}>
                                 <div>
                                     <p>Greffe de Cheveux</p>
                                     <div>
@@ -119,20 +86,37 @@ export default function AboutComp(props) {
                                 </div>
                             </ParallaxLayer>
 
-                    <ParallaxLayer id='dent' sticky={{ start: 3, end: 6 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
+                    <ParallaxLayer  id='dent'  sticky={{ start: 4, end: 6 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
                         <div className={`${styles.card} ${styles.sticky}`}>
                             <p>I'm a sticky layer 2 two</p>
                         </div>
                     </ParallaxLayer>
 
-                            <ParallaxLayer offset={4} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+                            <ParallaxLayer offset={4.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
                                 <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
                                     <p>2 Neither am I</p>
                                 </div>
                             </ParallaxLayer>
-                            <ParallaxLayer offset={5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+                            <ParallaxLayer offset={5.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
                                 <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
                                     <p>2 Neither am I</p>
+                                </div>
+                            </ParallaxLayer>
+
+                    <ParallaxLayer id='aesthetic'  sticky={{ start: 7, end: 9 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
+                        <div className={`${styles.card} ${styles.sticky}`}>
+                            <p>I'm a sticky layer 3 three</p>
+                        </div>
+                    </ParallaxLayer>
+
+                            <ParallaxLayer offset={7.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+                                <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
+                                    <p>3 Neither am I</p>
+                                </div>
+                            </ParallaxLayer>
+                            <ParallaxLayer offset={8.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+                                <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
+                                    <p>3 Neither am I</p>
                                 </div>
                             </ParallaxLayer>
                 </Parallax>
