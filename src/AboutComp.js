0e07a8b1c24509879e73a9a4c9cc9109fa2a934d
badr21b <badreddine@pembepanjur.com>
2022-06-18 04:44:44 +0300

    styling => wip

--- src/AboutComp.js
+++ src/AboutComp.js
@@ -6,6 +6,21 @@ import styles from './Style/styles.module.scss'
 import {translate} from "./functions/Languages/translation";
 import {HashLink} from "react-router-hash-link";
 
+import {Animated} from "react-animated-css";
+
+const Anchor = (props) => {
+  return (
+      <div>
+          <div>{props.anchorObj.resumeTitle}</div>
+          <div>{props.anchorObj.resumeText}</div>
+          <div>
+              <HashLink smooth className={"menu-list-item"} key={"hair"} to={"#" + props.anchorObj.key}>
+                  {props.anchorObj.readMoreButton}
+              </HashLink>
+          </div>
+      </div>
+  )
+}
 
 export default function AboutComp(props) {
 
@@ -18,16 +33,34 @@ export default function AboutComp(props) {
     });
 
 
-    const [treatmentsList, setTreatmentsList] = useState([
-        {
-            key: "hair", text:  translate("hair_transplant", storeLanguage), link: "/#hair",
-            subcategory: [
-                {key: "", text: "", }
-            ]
-        },
-        {key: "dent", text:  translate("dent_care", storeLanguage), link: "/#dent"},
-        {key: "aesthetic", text:  translate("aesthetic", storeLanguage), link: "/#aesthetic"},
-    ]);
+    const [currentTreatment, setCurrentTreatment] = useState("hair");
+
+    const [hairInfo] = useState({
+        key: "hair",
+        resumeTitle: "Greffe de Cheveux",
+        resumeText: "La perte de cheveux est acceptée comme un problème majeur affectant un pourcentage important de la population. Bien qu’il existe diverses raisons contribuant à la perte de cheveux, la cause la plus importante reste l’alopécie androgénétique qui est une affection héréditaire associée à la sensibilité des androgènes. Alors que les traitements non chirurgicaux disponibles peuvent aider à ralentir la perte de cheveux dans une certaine mesure, des progrès sans fin sur la restauration chirurgicale des cheveux ont transformé la greffe de cheveux en une méthode presque sûre et efficace, impeccable pour les patients affectés.\n" +
+            "\n" +
+            "La greffe de cheveux est un type de procédure en évolution constante, fournissant des solutions aux patients aux prises avec l’alopécie androgénétique ainsi que la perte de cheveux résultant d’autres sources telles que les traumatismes ou les troubles inflammatoires des cheveux. Les résultats d’apparence naturelle obtenus lors des opérations de greffe de cheveux agissent comme des facteurs encourageants, et chaque jour qui passe, de plus en plus d’hommes et de femmes optent pour la restauration des cheveux.",
+        readMoreButton: "En savoir plus"
+    });
+
+    const [dentInfo] = useState({
+        key: "dent",
+        resumeTitle: "Implants Dentaires",
+        resumeText: "Si vous envisagez un traitement par implants dentaires en Turquie, nous sommes votre clinique. Près de 20 ans d’expérience nous soutiennent ainsi que notre engagement envers l’innovation technologique, les meilleurs professionnels et une invasion chirurgicale minimale.\n" +
+            "\n" +
+            "Le manque de pièces dentaires a une solution, et notre équipe vous accompagnera tout au long du processus, depuis le diagnostic, le conseil et le résultat final de vos implants dentaires déjà posés en bouche.",
+        readMoreButton: "En savoir plus",
+    });
+
+    const [aestheticInfo] = useState({
+        key: "aesthetic",
+        resumeTitle: "Plastic surgery",
+        resumeText: "Face Surgery\n" + "Breast Surgery\n" + "Body Surgery\n" + "Bottox & Filler\n",
+        readMoreButton: "En savoir plus",
+    });
+
+    //https://clinicexpert.com/en/enlpps/?offer=aesthetic-eng&gclid=Cj0KCQjwzLCVBhD3ARIsAPKYTcRPTcc9EwFk5hR0Zv4OsYUq_v_7B003KliSXZtIlcid5l6_yaavTooaAgmiEALw_wcB
 
     // const [storeLanguage, setStoreLanguage] = useState(useSelector(state => state.language));
     // useEffect(() => {
@@ -46,12 +79,23 @@ export default function AboutComp(props) {
                 <Parallax pages={10}>
 
                     <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
-                        {/*<p className={styles.scrollText}>Scroll down</p>*/}
                         <div className={(isDesktop)? ("blocksContainer"):("blocksContainerMobile")} style={{height: "100vh", width: "100%"}}>
-                            {/*<div className={"backgroundDetailsContainer"}/>*/}
-                            <HashLink smooth className={"menu-list-item"} key={"hair"} to={"#hair"}> Hair anchor</HashLink>
-                            <HashLink smooth className={"menu-list-item"} key={"hair"} to={"#dent"}> dent anchor</HashLink>
-                            <HashLink smooth className={"menu-list-item"} key={"hair"} to={"#aesthetic"}> aesthetic anchor</HashLink>
+
+                            <div>
+                                <Animated animationIn="zoomInUp" animationOut="zoomOutDown" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
+                                    Hello World!!!
+                                </Animated>
+                                {/*<Anchor anchorObj={hairInfo}/>*/}
+                                {/*<Anchor anchorObj={dentInfo}/>*/}
+                                {/*<Anchor anchorObj={aestheticInfo}/>*/}
+                            </div>
+
+                            <div className={styles.categoryCardsListContainer}>
+                                <div>Hair</div>
+                                <div>Dent</div>
+                                <div>Aesthetics</div>
+                            </div>
+
                         </div>
                     </ParallaxLayer>
 
@@ -75,7 +119,7 @@ export default function AboutComp(props) {
                         </div>
                     </ParallaxLayer>
 
-                            <ParallaxLayer offset={1.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+                            <ParallaxLayer offset={1.8} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
                                 <div className={`${styles.card} ${styles.parallax} ${styles.blue_2}`}>
                                     <p>Transplantation Cheveux</p>
                                 </div>
@@ -86,12 +130,18 @@ export default function AboutComp(props) {
                                 </div>
                             </ParallaxLayer>
 
+
+
+
+
+
+
+
                     <ParallaxLayer  id='dent'  sticky={{ start: 4, end: 6 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
                         <div className={`${styles.card} ${styles.sticky}`}>
                             <p>I'm a sticky layer 2 two</p>
                         </div>
                     </ParallaxLayer>
-
                             <ParallaxLayer offset={4.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
                                 <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
                                     <p>2 Neither am I</p>
@@ -108,7 +158,6 @@ export default function AboutComp(props) {
                             <p>I'm a sticky layer 3 three</p>
                         </div>
                     </ParallaxLayer>
-
                             <ParallaxLayer offset={7.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
                                 <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
                                     <p>3 Neither am I</p>
@@ -121,50 +170,6 @@ export default function AboutComp(props) {
                             </ParallaxLayer>
                 </Parallax>
 
-
-
-                {/*<Parallax pages={2} style={{ top: '0', left: '0' }}>*/}
-                {/*    <ParallaxLayer*/}
-                {/*        offset={0}*/}
-                {/*        speed={2.5}*/}
-                {/*        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}*/}
-                {/*    >*/}
-                {/*        /!*<p>Scroll down</p>*!/*/}
-                {/*        /!*<div className={(isDesktop)? ("blocksContainer"):("blocksContainerMobile")} style={{height: "100vh", background: "red", width: "100%"}}>*!/*/}
-                {/*            <div className={"backgroundDetailsContainer"} style={{backgroundImage: categoryObj.backgroundUrl}}>*/}
-                {/*                <div className={"textBlockHolder"}>*/}
-                {/*                    <div className={"textBlockContainer"}>*/}
-                {/*                        <div className={"title"}>{categoryObj.title}</div>*/}
-                {/*                        <div className={"subtitle"}>{categoryObj.subtitle}</div>*/}
-                {/*                        <div className={"paragraph"}>{categoryObj.paragraph}</div>*/}
-                {/*                        <div className={"button"}>More info</div>*/}
-                {/*                    </div>*/}
-                {/*                </div>*/}
-                {/*            </div>*/}
-                {/*        /!*</div>*!/*/}
-                {/*    </ParallaxLayer>*/}
-
-                {/*    <ParallaxLayer offset={2} speed={2} style={{ backgroundColor: '#ff6d6d' }} />*/}
-
-                {/*    <ParallaxLayer*/}
-                {/*        offset={1}*/}
-                {/*        speed={0.5}*/}
-                {/*        style={{*/}
-                {/*            display: 'flex',*/}
-                {/*            justifyContent: 'center',*/}
-                {/*            alignItems: 'center',*/}
-                {/*            color: 'white',*/}
-                {/*        }}>*/}
-                {/*        <p>Scroll up</p>*/}
-                {/*        <div className={"categoryListContainer"}>*/}
-                {/*            <div className={"categoryBlock"}>Category</div>*/}
-                {/*            <div className={"categoryBlock"}>Category</div>*/}
-                {/*            <div className={"categoryBlock"}>Category</div>*/}
-                {/*            <div className={"categoryBlock"}>Category</div>*/}
-                {/*            <div className={"categoryBlock"}>Category</div>*/}
-                {/*        </div>*/}
-                {/*    </ParallaxLayer>*/}
-                {/*</Parallax>*/}
             </div>
         </>
 
